6518d51478aa6b6ece700a9010bbe7384859fc65
omokami <yamamoto.19940127@gmail.com>
2024-12-18 18:29:50 +0900

    Update MainLayout.tsx
    Co-Authored-By: Goldstar56 <191459902+goldstar56@users.noreply.github.com>

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header"
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            {/* <Header /> */}
+            <Header />
             {children}
         </>
     )
